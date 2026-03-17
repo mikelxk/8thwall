884018dd2156928c50cc512be949f133c7a4f405
Christoph Bartschat <christoph@8thwall.com>
2026-03-17 15:35:16 -0700

    [desktop] Uninstall express (#26)

--- apps/desktop/errors.ts
+++ apps/desktop/errors.ts
@@ -1,11 +1,5 @@
-import type {Request as ExpressRequest, Response as ExpressResponse, NextFunction} from 'express'
-
 import {makeJsonResponse} from './json-response'
 
-type NoReturnType = Promise<undefined | void> | undefined | void
-type Middleware = (
-  req: ExpressRequest, response: ExpressResponse, next: NextFunction
-) => NoReturnType
 type RequestHandler = (req: Request) => Response | Promise<Response>
 
 type CodedError = Error & {
@@ -18,28 +12,6 @@ const makeCodedError = (msg: string, code: number = 400): CodedError => {
   const err = new Error(msg) as CodedError
   err.code = code
   return err
-}
-
-const returnCodedError = (
-  res: ExpressResponse,
-  err: CodedError,
-  defaultMessage = defaultErrorMessage
-): void => {
-  if (err.code && typeof err.code === 'number') {
-    res.status(err.code).json({message: err.message})
-    return
-  }
-  // eslint-disable-next-line no-console
-  console.error('Unexpected error:', err)
-  res.status(500).json({message: defaultMessage})
-}
-
-const withErrorHandling = (handler: Middleware): Middleware => async (req, res, next) => {
-  try {
-    await handler(req, res, next)
-  } catch (err) {
-    returnCodedError(res, err as CodedError, `Unexpected Error on: ${req.originalUrl}`)
-  }
 }
 
 const returnCodedErrorResponse = (err: CodedError, defaultMessage = defaultErrorMessage) => {
@@ -62,6 +34,5 @@ const withErrorHandlingResponse = (handler: RequestHandler) => async (req: Reque
 
 export {
   makeCodedError,
-  withErrorHandling,
   withErrorHandlingResponse,
 }
